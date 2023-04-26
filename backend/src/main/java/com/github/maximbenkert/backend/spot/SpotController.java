1003729dc189a2c03c68e9a049464c674003256b
MaximBenkert <126075845+MaximBenkert@users.noreply.github.com>
2023-04-26 13:40:16 +0200

    delete logo from frontend

--- backend/src/main/java/com/github/maximbenkert/backend/spot/SpotController.java
+++ backend/src/main/java/com/github/maximbenkert/backend/spot/SpotController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/spot")
+@RequestMapping("/api/spots")
 @RequiredArgsConstructor
 public class SpotController {
     private final SpotService spotService;
