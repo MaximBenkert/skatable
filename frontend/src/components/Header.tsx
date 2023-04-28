40b31e9ae098d6d5ab524a1cffb3f98469a68335
MaximBenkert <126075845+MaximBenkert@users.noreply.github.com>
2023-04-28 15:43:16 +0200

    change Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,8 +3,9 @@ import {Typography} from "@mui/material";
 export default function Header() {
 
     return(
-            <Typography variant="h1" component="h1" sx={{fontSize: "2rem"}}>
+            <Typography variant="h1" component="h1" sx={{fontSize: "3rem"}}>
                 Skatable
+                <h2>Spots</h2>
             </Typography>
     )
 
