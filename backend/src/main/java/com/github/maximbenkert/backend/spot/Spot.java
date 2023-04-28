186a7d4fc1ce4fea1ed99731e7810be36f3d3a3f
MaximBenkert <126075845+MaximBenkert@users.noreply.github.com>
2023-04-28 11:41:23 +0200

    Model: dadd Annotations NotBlank and Size add Constructor with id = null add function withId

--- backend/src/main/java/com/github/maximbenkert/backend/spot/Spot.java
+++ backend/src/main/java/com/github/maximbenkert/backend/spot/Spot.java
@@ -3,11 +3,25 @@ package com.github.maximbenkert.backend.spot;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 @Document("spots")
 public record Spot(
         @Id
         String id,
+        @NotBlank
+        @Size (min = 3, max = 32)
         Coordinates coordinates,
         String name
 ) {
+        Spot(
+                Coordinates coordinates,
+                String name
+        ){
+                this(null, coordinates, name);
+        }
+        public Spot withId(String id) {
+                return new Spot (id, coordinates, name);
+        }
 }
