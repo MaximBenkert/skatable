876ab9d6a73c8a6ffae88310a0184b82b522944a
MaximBenkert <126075845+MaximBenkert@users.noreply.github.com>
2023-05-02 14:16:03 +0200

    styling header

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -12,6 +12,13 @@ export default function Navigation() {
 
             <Paper sx={{position: 'fixed', bottom: '0.1rem'}}>
                 <BottomNavigation sx={{width: '100vw', bgcolor: "#228B22"}}>
+                    <NavLink to="/spots">
+                        <BottomNavigationAction
+                            label="spots"
+                            value="/spots"
+                            icon={<HomeIcon/>}
+                        />
+                    </NavLink>
                     <NavLink to="/add">
                         <BottomNavigationAction
                             label="add"
@@ -19,18 +26,6 @@ export default function Navigation() {
                             icon={<AddIcon/>}
                         />
                     </NavLink>
-
-                    <NavLink to="/spots">
-                        <BottomNavigationAction
-                            label="spots"
-                            value="/spots"
-                            icon={<HomeIcon/>}
-                        />
-
-
-                    </NavLink>
-
-
                 </BottomNavigation>
             </Paper>
 
