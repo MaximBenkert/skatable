a2b0b0d28752914686e0c89392572a08a41f0b11
MaximBenkert <126075845+MaximBenkert@users.noreply.github.com>
2023-05-03 15:13:55 +0200

    styled SpotDetails

--- frontend/src/components/SpotDetails.tsx
+++ frontend/src/components/SpotDetails.tsx
@@ -1,7 +1,7 @@
 import {Spot} from "../models/Spot";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import React, {useEffect} from "react";
-import {Typography} from "@mui/material";
+import {Box, Button, ButtonGroup, Card, Container, Typography} from "@mui/material";
 
 type DetailProps = {
     loadSpotById: (id: string) => void;
@@ -10,6 +10,7 @@ type DetailProps = {
 
 export default function SpotDetails(props: DetailProps) {
     const {id} = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (id) {
@@ -19,20 +20,31 @@ export default function SpotDetails(props: DetailProps) {
     }, [id])
 
     return (
-        <div>
-            {
-                props.spot
-                    ? <div>
-                        <Typography>{props.spot.id}</Typography>
-                        <Typography>{props.spot.name}</Typography>
-                        <Typography>{props.spot.coordinates.latitude}</Typography>
-                        <Typography>{props.spot.coordinates.longitude}</Typography>
+        <Container maxWidth="lg">
+            <Box sx={{bgcolor: '#efebe9', p: "1rem", pb: "3rem"}}>
+                <Typography sx={{fontSize: "1.5rem", padding: "1rem"}} variant="h2" component="h2">
+                    Delivery Details
+                </Typography>
+                {props.spot ?
+                    <Card variant="outlined" sx={{p: "0.8rem"}}>
+                        <small>ID: {props.spot.id}</small>
+                        <p>Spotname: {props.spot.name}</p>
+                        <p>Latitude: {props.spot.coordinates.latitude}</p>
+                        <p>Longitude: {props.spot.coordinates.longitude}</p>
+                        <ButtonGroup sx={{display: "flex", justifyContent: "space-between"}} variant="text"
+                                     aria-label="text button group">
+                            <Button variant="outlined"
+                                    onClick={() => navigate(`/`)}>Back</Button>
+                        </ButtonGroup>
 
-                    </div>
-                    : <div>... loading </div>
-            }
+                    </Card>
+                    :
+                    <p>... loading</p>}
+            </Box>
+        </Container>
 
-        </div>
     )
 
 }
+
+
