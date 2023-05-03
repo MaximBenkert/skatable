7925b9d4c6b6daa3f4f66bb43e13d804e7726eb6
MaximBenkert <126075845+MaximBenkert@users.noreply.github.com>
2023-05-03 14:38:31 +0200

    frontend - create loadSpotById in hooks

--- frontend/src/hooks/useSpots.ts
+++ frontend/src/hooks/useSpots.ts
@@ -1,10 +1,12 @@
 import {useEffect, useState} from "react";
 import {NewSpot, Spot} from "../models/Spot";
 import axios from "axios";
+import {toast} from "react-toastify";
 
 export default function useSpots() {
 
     const [spots, setSpots] = useState<Spot[]>([])
+    const [spot, setSpot] = useState<Spot>({id: "", coordinates: {latitude: 0, longitude: 0}, name: ""})
 
     useEffect(() => {
         loadAllSpots()
@@ -18,6 +20,16 @@ export default function useSpots() {
             .catch(reason => console.error(reason))
     }
 
+    function loadSpotByID(id: string) {
+        axios.get("/api/spots/" + id)
+            .then((response) => {
+                setSpot(response.data)
+            })
+            .catch((error) => {
+                toast.error("Spot not found!")
+            })
+    }
+
         function addSpot(spot: NewSpot) {
             axios.post("/api/spots", spot)
                 .then(spotAddedResponse => spotAddedResponse.data)
@@ -25,5 +37,5 @@ export default function useSpots() {
                 .catch(reason => console.error(reason))
         }
 
-    return {spots, addSpot}
+    return {spot, spots, addSpot, loadSpotByID}
 }
