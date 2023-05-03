7a53a177e70a211b5b12acb94f29bffcfb1bcd9c
MaximBenkert <126075845+MaximBenkert@users.noreply.github.com>
2023-05-03 14:43:02 +0200

    SpotCard adjusted for Details

--- frontend/src/components/SpotCard.tsx
+++ frontend/src/components/SpotCard.tsx
@@ -1,19 +1,27 @@
 import {Spot} from "../models/Spot";
-import {Card} from "@mui/material";
+import {Button, ButtonGroup, Card} from "@mui/material";
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 type CardProps = {
     spot: Spot
 }
 
 export default function SpotCard(props: CardProps) {
+    const navigate = useNavigate()
 
     return (
-        <Card variant="outlined">
 
-            <p><big>{props.spot.name}</big></p>
-            <small>Coordinates: {props.spot.coordinates.latitude} </small>
-            <small>Coordinates: {props.spot.coordinates.longitude} </small>
-        </Card>
+
+    <Card variant="outlined" className="details-card">
+        <small> ID: {props.spot.id}</small>
+        <p>Title: {props.spot.name}</p>
+        <ButtonGroup sx={{display: "flex", justifyContent: "space-between"}} variant="text"
+                     aria-label="text button group">
+            <Button variant="outlined"
+                    onClick={() => navigate(`/details/${props.spot.id}`)}>Details</Button>
+        </ButtonGroup>
+    </Card>
     )
 }
+
